0a3ac9258738aff1cde51d9f7d229702dae276e1
Shreemanarjun <Shreemanarjunsahu@gmail.com>
2026-03-21 01:15:03 +0530

    chore: remove `test_type.dart` and `test_type_check.dart`

--- test_type.dart
+++
@@ -1,6 +0,0 @@
-import 'package:formix/formix.dart';
-void main() {
-  const ageField = FormixFieldID<int>('age');
-  var field = const FormixField<int>(id: ageField, initialValue: 0);
-  print(field.isTypeValid('wrong type'));
-}
